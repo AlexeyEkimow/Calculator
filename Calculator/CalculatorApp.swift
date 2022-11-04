99a2d4045ad5a6264824742d8a985209d4c0eb7c
Alexey Ekimow <Ekimow1994@yandex.ru>
2022-11-05 00:24:40 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alexey Ekimow on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
